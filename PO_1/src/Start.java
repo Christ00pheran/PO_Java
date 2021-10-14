9381bbff9f39e3b4d26420bc039543d0038dea4c
Christ00pher <christ00pher@vp.pl>
2021-10-14 13:30:39 +0200

    1

---
+++ PO_1/src/Start.java
@@ -0,0 +1,39 @@
+public class Start{
+
+    public static void main(String[] args){
+        System.out.println("Hello world");
+        //zad1
+        System.out.println(31+29+31);
+        //zad2
+        System.out.println(1+2+3+4+5+6+7+8+9+10);
+        //zad3
+        System.out.println(1*2*3*4*5*6*7*8*9*10);
+        //zad4
+        int a=1000;
+        System.out.println("Po pierwszym roku: "+a*1.06);
+        System.out.println("Po drugim roku: "+a*1.06*1.06);
+        System.out.println("Po trzecim roku: "+a*1.06*1.06*1.06);
+        //zad5
+        System.out.println("+____+");
+        System.out.println("|Java|");
+        System.out.println("+----+");
+        //zad6
+        System.out.println("   /////  ");
+        System.out.println(" +'''''''+ ");
+        System.out.println("(|  o o  |)");
+        System.out.println("   | ^ |   ");
+        System.out.println("  | \u2018_\u2019 |   ");
+        System.out.println("  +-----+");
+        //zad10
+        System.out.println("Mad Max: Fury Road\nThe Fast and the Furious: Tokyo Drift\nInterstellar\n");
+        //zad11
+        System.out.println("Dwadzieścia siedem kości,\n" +
+                "trzydzieści pięć mięśni,\n" +
+                "około dwóch tysięcy komórek nerwowych\n" +
+                "w każdej opuszce naszych pięciu palców.\n" +
+                "To zupełnie wystarczy,\n" +
+                "żeby napisać „Mein Kampf”\n" +
+                "albo „Chatkę Puchatka”.");
+    }
+}
+//1,2,3,4,5,6-zamienic jeden ze znakow na unicode,bez 7,8,9,12
