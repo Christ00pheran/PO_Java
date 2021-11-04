6307052742a6c05c6569d37c42c97bd7a6c4703a
Christ00pheran <59085653+Christ00pheran@users.noreply.github.com>
2021-11-04 23:12:14 +0100

    Update Start.java

--- PO_4/src/Start.java
+++ PO_4/src/Start.java
@@ -1,31 +1,38 @@
 import java.lang.reflect.Array;
+import java.math.BigDecimal;
+import java.math.BigInteger;
+import java.math.RoundingMode;
 import java.util.Arrays;
 import java.util.Scanner;
 
 public class Start {
     public static void main(String[] args){
-    System.out.println("Liczba wystapien c: "+countChar("ccc", 'c'));
-    System.out.println("Liczba wystąpień substringa: "+countSubStr("abcabcabc","abc"));
-    Scanner reader=new Scanner(System.in);
-    System.out.println("Podaj stringa (middle): ");
-    String a = reader.next();
-    System.out.println("Srodek: "+middle(a));
-    System.out.println("Podaj stringa (repeat): ");
-    String b = reader.next();
-    System.out.println("Repeat: "+repeat(b, 3));
+        System.out.println("Liczba wystapien c: "+countChar("ccc", 'c'));
+        System.out.println("Liczba wystąpień substringa: "+countSubStr("abcabcabc","abc"));
+        Scanner reader=new Scanner(System.in);
+        System.out.println("Podaj stringa (middle): ");
+        String a = reader.next();
+        System.out.println("Srodek: "+middle(a));
+        System.out.println("Podaj stringa (repeat): ");
+        String b = reader.next();
+        System.out.println("Repeat: "+repeat(b, 3));
 //    System.out.println("Where: "+where("abcabcabc", "a"));
         System.out.println("Change: "+change("aBc"));
+        System.out.println(seeds(2));
+        System.out.println(kapital(1000, 0.04,3));
+        System.out.println("Seed: "+nice("abcabc"));
+        System.out.println("Seed2: "+nice2("abcabc", ',',3));
 
     }
     //zad1.a
     public static int countChar(String str, char c){
-    int suma=0;
-    for(int i=0; i<str.length(); i++){
-        if(str.charAt(i)==c){
-            suma+=1;
+        int suma=0;
+        for(int i=0; i<str.length(); i++){
+            if(str.charAt(i)==c){
+                suma+=1;
+            }
         }
-    }
-    return suma;
+        return suma;
     }
     //zad1.b
     public static int countSubStr(String str, String subStr){
@@ -40,9 +47,9 @@ public class Start {
                 last+=subStr.length();
             }
         }
-    return suma;
-        }
-        //zad1.c
+        return suma;
+    }
+    //zad1.c
     public static String middle(String str){
         int len=str.length();
         String wynik="";
@@ -68,7 +75,7 @@ public class Start {
         }
         return wynik;
     }
-//zad1.e
+    //zad1.e
 //public static int[] where(String str, String subStr){
 //    int[] wynik=new int[0];
 //    int last=0;
@@ -106,5 +113,55 @@ public class Start {
         }
         return wynik;
     }
-}
+    //zad1.g
+    public static String nice(String str) {
+        StringBuffer strt = new StringBuffer(str);
+        for(int i=(strt.length()-3); i>0;i=i-3) {
+            strt.insert(i,"\'");
+        }
+        return strt.toString();
+    }
+    //zad1.h
+    public static String nice2(String str, char sep, int x) {
+        StringBuffer strt = new StringBuffer(str);
+        for(int i=(strt.length()-x);i>0;i-=x) {
+            strt.insert(i,sep);
+        }
+        return strt.toString();
+    }
+    //zad4
+    public static BigInteger seeds(int n){
+        int s = n * n;
+        BigInteger suma = new BigInteger("0");
+        int se=1;
 
+
+        for (int i = 0; i < s; i++) {
+            BigInteger big=BigInteger.valueOf(se);
+
+            suma=suma.add(big);
+
+            se*=2;
+
+        }
+        return suma;
+    }
+    //zad5
+    public static BigDecimal kapital(int k, double p, int n){
+        BigDecimal suma= new BigDecimal("0");
+        BigDecimal big=BigDecimal.valueOf(k);
+        suma=suma.add(big);
+        BigDecimal pro = BigDecimal.valueOf(p);
+
+        for(int i=0; i<n; i++){
+
+        BigDecimal t=suma.multiply(pro);
+        suma=suma.add(t);
+
+
+
+        }
+        suma=suma.setScale(2, RoundingMode.CEILING);
+    return suma;
+    }
+}
